b75e362bf1718e6d9d591e8e68e30422ccf60cd9
Franck R <franck@status.im>
2022-04-01 17:23:25 +1100

    Add privacy friendly analytics (#13)

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ const social = document.querySelector('#social-box');
 const socialMobile = document.querySelector('#social-box-mobile');
 const logo = document.querySelector('#header-logo');
 
-function handleHeader () {
+function handleHeader() {
     social.classList.toggle('social--visible');
     socialMobile.classList.toggle('social-mobile--visible');
     logo.classList.toggle('logo--hidden');
@@ -13,3 +13,17 @@ function handleHeader () {
 }
 
 burger.addEventListener('click', handleHeader);
+
+(function (f, a, t, h, o, m) {
+    a[h] = a[h] || function () {
+        (a[h].q = a[h].q || []).push(arguments)
+    };
+    o = f.createElement('script');
+    m = f.getElementsByTagName('script')[0];
+    o.async = 1;
+    o.src = t;
+    o.id = 'fathom-script';
+    m.parentNode.insertBefore(o, m)
+})(document, window, '//fathom.status.im/tracker.js', 'fathom');
+fathom('set', 'siteId', 'BBYCC');
+fathom('trackPageview');
