260d454fb06d8291eeda3e4c7a0dd1e36a536316
jinhojang6 <jinho5051@gmail.com>
2023-10-14 01:48:24 +0900

    feat: remove fathom

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,13 +57,6 @@ const config = {
         path: 'about',
       },
     ],
-    [
-      '@acid-info/docusaurus-fathom',
-      {
-        siteId: 'BBYCC',
-        scriptUrl: 'https://fathom.status.im/tracker.js',
-      },
-    ],
   ],
 
   themeConfig:
