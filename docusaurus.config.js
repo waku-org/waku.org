b0b2aae6e3366a3b4ce77f060f5e7497749c6050
jinhojang6 <jinho5051@gmail.com>
2024-05-16 22:24:13 +0900

    feat: add security page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -188,6 +188,10 @@ const config = {
                 href: '/rules-of-engagement',
                 label: 'Rules of Engagement',
               },
+              {
+                href: '/security',
+                label: 'Security',
+              },
             ],
           },
         ],
