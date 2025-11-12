5822e28e5c2a738235598b02e291131870f9b7aa
jinhojang6 <jinho5051@gmail.com>
2025-11-12 18:36:31 +0900

    feat: add TopToast component for notifications

---
+++ src/components/TopToast/TopToast.tsx
@@ -0,0 +1,44 @@
+// src/components/TopToast.js
+import React from 'react'
+import styles from './TopToast.module.css'
+import ThemedImage from '@theme/ThemedImage'
+import Link from '@docusaurus/Link'
+
+type TopToastProps = {
+  setShowTopToast: (showTopToast: boolean) => void
+}
+
+const TopToast = ({ setShowTopToast }: TopToastProps) => {
+  const handleClose = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    e.stopPropagation()
+    setShowTopToast(false)
+  }
+
+  return (
+    <Link href="https://logos.co" target="_blank">
+      <div className={styles.toast}>
+        <span className={styles.message}>
+          <span>
+            Waku is now Logos Messaging - this site will redirect to
+            https://logos.co by the end of 2025. Join us there for more info on
+            our node program and testnet.
+          </span>
+        </span>
+        <div>
+          <button className={styles.closeButton} onClick={handleClose}>
+            <ThemedImage
+              alt="Close"
+              sources={{
+                light: '/icons/close-white.svg',
+                dark: '/icons/close-black.svg',
+              }}
+            />
+          </button>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default TopToast
