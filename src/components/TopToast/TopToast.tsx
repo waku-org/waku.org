e46dc1e97014277711466393dc4c06a8368ba6b5
jinhojang6 <jinho5051@gmail.com>
2025-11-14 19:38:52 +0900

    fix: update TopToast message for clarity and engagement

--- src/components/TopToast/TopToast.tsx
+++ src/components/TopToast/TopToast.tsx
@@ -21,8 +21,8 @@ const TopToast = ({ setShowTopToast }: TopToastProps) => {
         <span className={styles.message}>
           <span>
             Waku is now Logos Messaging - this site will redirect to
-            https://logos.co by the end of 2025. Join us there for more info on
-            our node program and testnet.
+            https://logos.co by the end of 2025. Join us there to take part in
+            our testnet node programme.
           </span>
         </span>
         <div>
